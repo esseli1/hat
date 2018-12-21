82b5a3608138378afc37b3a92709f61884001322
esseli1 <46043777+esseli1@users.noreply.github.com>
2018-12-21 19:20:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
+const Discord = require('discord.js');
+const Canvas = require('canvas')
+const client = new Discord.Client();
+const prefix = '+';
+
 cssclient.on("ready", () => {
   function blue() {
     client.guilds.get('475064825890734085').roles.find("name", "R̷a̷i̷n̷b̷o̷w̷").setColor("RANDOM");
   };
   setInterval(blue, 3000);
 });
+
 client.login(process.env.BOT_TOKEN);
